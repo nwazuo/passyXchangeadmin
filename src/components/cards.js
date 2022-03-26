2ff36a0a16463c9148ea8c4c7772bf67e43fe5ef
Chizo Nwazuo <chizonwazuo@yahoo.com>
2022-03-26 23:01:09 +0000

    fun: make infinite loading work, add skeletons, refactor

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,76 +1,104 @@
-import Card from "./card";
-import { Container, Grid } from "@mui/material";
-import { useQuery } from "react-query";
-import axios from "axios";
-import { useState, useEffect, useRef } from "react";
+import Card from './card';
+import { Container, Grid, Skeleton } from '@mui/material';
+import { useInfiniteQuery, useQuery, useQueryClient } from 'react-query';
+import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 
-let blockAccount = "3LrQZZkVyrR2bUsos8kcVUyr9N5pFvr1U7";
+// I don't know how sensitive this is. If it is indeed sensitive,
+// reference it as an environment variable
+let blockAccount = '3LrQZZkVyrR2bUsos8kcVUyr9N5pFvr1U7';
+
+// skeleton
+const CardsSkeleton = () => (
+  <Grid container>
+    <Grid item xs={12} md={6}>
+      <Skeleton height={200} sx={{ marginX: '10px' }} />
+    </Grid>
+    <Grid item xs={12} md={6}>
+      <Skeleton height={200} sx={{ marginX: '10px' }} />
+    </Grid>
+  </Grid>
+);
 
 const Cards = () => {
-  const fetchData = () => {
-    // setLoading(true);
-    return axios.get(
-      `http://passyblockchain-admin.herokuapp.com/api/v1/addresses/${blockAccount}?pageNumber=${page}&pageSize=5&sort=-1`
-    );
-  };
+  const { ref, inView } = useInView();
 
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [transactions, setTransactions] = useState([]);
-
-  const pageEnd = useRef();
-
-  const loadMore = () => {
-    setPage((oldPrev) => oldPrev + 1);
-  };
-  const onSucesss = () => {
-    setLoading(true);
-    setTransactions([...transactions, ...data?.data?.transactions?.data]);
-  };
-  const { data, isLoading } = useQuery(
-    ["fetchData", page],
-    () => fetchData(page),
+  // useInfiniteQuery is a module that's more suited to
+  // doing infinite scroll UIs
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'addresses',
+    async ({ pageParam = 1 }) => {
+      const res = await axios.get(
+        `http://passyblockchain-admin.herokuapp.com/api/v1/addresses/${blockAccount}?pageNumber=${pageParam}&pageSize=6&sort=-1`
+      );
+      return res.data;
+    },
     {
-      onSuccess: () => onSucesss(),
+      // check if the recently returned data is the
+      // last set of data
+      // return undefined to indicate last page
+      getNextPageParam: (lastPage) =>
+        lastPage.transactions.pageNumber >= lastPage.transactions.totalPages &&
+        undefined,
     }
   );
 
-  // console.log(data);
-  let num = 1;
+  /* 
+    register effect to use intersection observer to check
+    when the user has scrolled to bottom of page
+  */
   useEffect(() => {
-    if (loading) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            num++;
-            loadMore();
-            if (num >= data.data.transactions.totalPages) {
-              observer.unobserve(pageEnd.current);
-            }
-          }
-        },
-        { threshold: 1 }
-      );
-
-      observer.observe(pageEnd.current);
+    if (inView) {
+      fetchNextPage();
     }
-  }, [loading, num]);
-
-  if (isLoading) return <h1>loading Data</h1>;
+  }, [inView]);
 
   return (
     <Container>
-      <Grid container>
-        {transactions?.map((dta, i) => (
-          <Grid item xs={12} md={6}>
-            <Card data={dta} key={i} />
+      {status === 'loading' ? (
+        <>
+          <CardsSkeleton />
+          <CardsSkeleton />
+          <CardsSkeleton />
+        </>
+      ) : status === 'error' ? (
+        <p>Something went wrong...[{error.message}]</p>
+      ) : (
+        <>
+          <Grid container>
+            {data.pages.map((page, i) => (
+              <React.Fragment key={i}>
+                {page.transactions.data.map((transaction) => (
+                  <Grid item xs={12} md={6}>
+                    <Card data={transaction} key={transaction.hash} />
+                  </Grid>
+                ))}
+              </React.Fragment>
+            ))}
+            {isFetchingNextPage && <CardsSkeleton />}
           </Grid>
-        ))}
-      </Grid>
-
-      <button onClick={loadMore} ref={pageEnd}>
-        Load More
-      </button>
+          <button
+            onClick={fetchNextPage}
+            ref={ref}
+            disabled={isFetchingNextPage || !hasNextPage}
+          >
+            {isFetchingNextPage
+              ? 'Loading more...'
+              : hasNextPage
+              ? 'Load more'
+              : 'Nothing more to load'}
+          </button>
+        </>
+      )}
     </Container>
   );
 };
--- src/components/kingsCardMain.js
+++ src/components/kingsCardMain.js
@@ -1,53 +1,78 @@
-import { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-import KingComponent from "./kingCard";
-import { useTheme } from "@emotion/react";
-import axios from "axios";
-import { useQuery } from "react-query";
+import { useState, useEffect } from 'react';
+import { Box, Skeleton } from '@mui/material';
+import KingComponent from './kingCard';
+import { useTheme } from '@emotion/react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
-let blockAccount = "3LrQZZkVyrR2bUsos8kcVUyr9N5pFvr1U7";
+let blockAccount = '3LrQZZkVyrR2bUsos8kcVUyr9N5pFvr1U7';
+
+/* I moved this out of the component to prevent         re-declaring it on every render
+ This may not have any performance effects but its good practice
+ Other alternatives to this can be either to
+ pass the function value directly to useQuery or
+ to include it in the component but wrap it with
+ a React.useMemo() hook(for memoization)
+*/
+const fetchData = () => {
+  return axios.get(
+    `http://passyblockchain-admin.herokuapp.com/api/v1/addresses/${blockAccount}/`
+  );
+};
+
+const KingsSkeleton = () => (
+  <>
+    <Skeleton
+      width="100%"
+      sx={{ margin: '25px', marginX: '34px', padding: '20px' }}
+    >
+      <h6>Infer height</h6>
+      <h1>Inferrr</h1>
+    </Skeleton>
+    <Skeleton
+      width="100%"
+      sx={{ margin: '25px', marginX: '34px', padding: '20px' }}
+    >
+      <h6>Infer height</h6>
+      <h1>Inferrr</h1>
+    </Skeleton>
+  </>
+);
 
 const KingsCardMain = () => {
-  let blockAccount = "3LrQZZkVyrR2bUsos8kcVUyr9N5pFvr1U7";
-  const fetchData = () => {
-    return axios.get(
-      `http://passyblockchain-admin.herokuapp.com/api/v1/addresses/${blockAccount}/`
-    );
-  };
-
   const theme = useTheme();
-  const { data, isLoading } = useQuery("fetchData", () => fetchData());
-  //console.log(data);
+  const { data, isLoading } = useQuery('fetchData', fetchData);
 
-  const [inputParams, setInputParams] = useState([
+  const inputParams = [
     {
-      color: "#00347A",
-      subText: "Total Received",
+      color: '#00347A',
+      subText: 'Total Received',
       mainText: `${data?.data?.totalReceived / 100000000}`,
     },
     {
-      color: "#0196D8",
-      subText: "Total Sent",
+      color: '#0196D8',
+      subText: 'Total Sent',
       mainText: `${data?.data?.totalSent / 100000000}`,
     },
-  ]);
-  if (isLoading) return <h1>loading Data</h1>;
+  ];
 
   return (
     <>
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          [theme.breakpoints.down("sm")]: {
-            flexDirection: "column",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          [theme.breakpoints.down('sm')]: {
+            flexDirection: 'column',
           },
         }}
       >
-        {inputParams?.map((param, i) => (
-          <KingComponent {...param} key={i} />
-        ))}
+        {isLoading ? (
+          <KingsSkeleton />
+        ) : (
+          inputParams?.map((param, i) => <KingComponent {...param} key={i} />)
+        )}
       </Box>
     </>
   );
